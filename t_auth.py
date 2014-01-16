cff0e5e3e5c20cdc419cc86a7889be1f1ad070f9
shkr <shashank.f1@gmail.com>
2014-01-16 02:18:38 -0500

    Initialized tweet-event

---
+++ t_auth.py
@@ -0,0 +1,4 @@
+auth = dict(consumer_key = 'xqwwrn8oA9iwQf2ifORmog',
+          consumer_secret = 'zuK8s7MRZXezV1mkOb9JNm4yYK8g54bTBl5tFVnic',
+          access_token = '2193603675-Q667mtzACPnK3HWACndAQhEQ5CwFHsFTqUvkL6h',
+          access_token_secret = '7ZsbZBlO3mGdbSeMtEGt6FwV1iIQBXX8YFQQMVZylMwl7')
