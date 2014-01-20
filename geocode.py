8a2e40dc3a0b9ef2d91e1b91d850cd4ac055e2b0
Shashank Shekhar <shashank.f1@gmail.com>
2014-01-20 14:29:03 -0500

    Update geocode.py
    Source : Map.google.api added in file

--- geocode.py
+++ geocode.py
@@ -1,5 +1,7 @@
 ###Locations and their geocode details###
 
+#For more geocodes use :  "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false"%place_name
+
 location = {'Delhi':dict(latitude=28.6353080,longitude= 77.2249600,radius=25),'Mumbai':dict(latitude=19.0759837,longitude=72.8776559,radius=25),'NYC':dict(latitude=40.7143528,longitude=-74.00597309999999,radius=25),
             'Boston':dict(latitude=42.3988669,longitude=-70.9232009999999,radius=25),'SanFrancisco':dict(latitude=37.7749295,longitude=-122.4194155,radius=25),'London':dict(latitude=51.6723432,longitude=-0.1998244,radius=25),'LosAngeles':dict(latitude=34.337306,longitude=-118.155289,radius=25),'Toronto':dict(latitude=43.5810847000001,longitude=-79.639219,radius=25),'SanDiego':dict(latitude=32.7153292,longitude=-117.1572551,radius=25),'Houston':dict(latitude=29.7601927,longitude=-95.36938959999999,radius=25)}
 
