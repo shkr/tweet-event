e84235ba63a5955b4e79a79f0563a65bc28a0213
shkr <shashank.f1@gmail.com>
2014-03-23 22:44:24 -0400

    TweepyListerner try-catch edited

--- TweepyListener.py
+++ TweepyListener.py
@@ -33,7 +33,7 @@ class Listener(StreamListener):
             self.err_table = prefix+'ERROR_LOG'
             errcurr = conn.cursor()
             #Table definition
-            errcur.execute("CREATE TABLE IF NOT EXISTS %sTable (Id INT PRIMARY KEY AUTO_INCREMENT,status_id VARCHAR(50),type VARCHAR(50),time BIGINT)"%self.err_table)
+            errcurr.execute("CREATE TABLE IF NOT EXISTS %sTable (Id INT PRIMARY KEY AUTO_INCREMENT,status_id VARCHAR(50),type VARCHAR(50),time BIGINT)"%self.err_table)
             errcurr.close()
 
     def verboseprint(self,*args):
